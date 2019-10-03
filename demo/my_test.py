1ada1c9e1a10325caab39b0392d4be8d366ddced
shekhar verma <shekharvrm47@gmail.com>
2019-10-03 13:29:17 +0530

    added progress sheet

--- demo/my_test.py
+++ demo/my_test.py
@@ -23,7 +23,7 @@ print("Test Directory path",test_images_path)
 imagepaths=[os.path.join(test_images_path,f) for f in os.listdir(test_images_path)]
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT ='frozen_inference_graph.pb'
+PATH_TO_CKPT ='frozen_inference_graph_11213.pb'
 
 # List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = 'label_map.pbtxt'
@@ -67,6 +67,7 @@ with detection_graph.as_default():
             image_np = cv2.imread(imagepath)
             # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
             image_np_expanded = np.expand_dims(image_np, axis=0)
+            
             # Extract image tensor
             image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
             # Extract detection boxes
@@ -75,13 +76,23 @@ with detection_graph.as_default():
             scores = detection_graph.get_tensor_by_name('detection_scores:0')
             # Extract detection classes
             classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            
             # Extract number of detectionsd
             num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+            
             # Actual detection.
             (boxes, scores, classes, num_detections) = sess.run([boxes, scores, classes, num_detections],feed_dict={image_tensor: image_np_expanded})
+            print("boxes = ",boxes) ##may be coordinates
+            print("scores = ",scores)  ##confidence score
+            print("classes = ",classes) ##detected classes
+            print("num_detections = ",num_detections) ##no of detections in the image 
+            
+            
             # Visualization of the results of a detection.
             vis_util.visualize_boxes_and_labels_on_image_array(image_np,np.squeeze(boxes),np.squeeze(classes).astype(np.int32),np.squeeze(scores),
-                category_index,use_normalized_coordinates=True,line_thickness=8)
+                category_index,use_normalized_coordinates=True,line_thickness=4)
+            
+            
 
             # Display output
             cv2.imshow('object detection', cv2.resize(image_np, (800, 600)))
