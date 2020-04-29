302b3a311c2d1f418d907ad34063c84d6d246f0d
shekhuverma <shekharvrm47@gmail.com>
2020-04-29 21:29:16 +0530

    Added a script to save the results

--- image_processing/lane_detection_result.py
+++ image_processing/lane_detection_result.py
@@ -4,7 +4,7 @@ import numpy as np
 import concurrent.futures
 from threading import Timer, Event
 
-kernel1 = np.ones((3,3),np.uint8)
+
 ##cap = cv2.VideoCapture("I:\\Minor_Project\\Workflow\\demo\\video.mp4")
 
 cap = cv2.VideoCapture("I://Minor_Project//Workflow//demo//video_1.mp4")
@@ -27,6 +27,13 @@ coordinates=np.array(([[[0,height-40],[width,height-40],[0,340],[width,340]]]),d
 
 coordinates2=np.copy(coordinates)
 
+i=1
+#To save the images
+def capture(frame_list):
+
+	for l,frame in enumerate(frame_list, start=1):
+		cv2.imwrite((str(i)+"_"+str(l)+str(".jpg")),frame)
+	
 
 def roi(image,coordinates):
 	mask=np.zeros(image.shape,dtype='uint8')
@@ -97,39 +104,25 @@ if __name__=='__main__':
 	while True:
 		last_time=time.time()
 		ret,frame=cap.read()
+		frame_final_output=np.copy(frame)
 		frame_cvt=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 		frame_cvt2=np.copy(frame_cvt)
-		frame_cvt=cv2.bitwise_and(frame_cvt,frame_cvt,mask=mask)
-		frame_cvt=cv2.GaussianBlur(frame_cvt,(3,3),0)
+		frame_cvt3=cv2.bitwise_and(frame_cvt,frame_cvt,mask=mask)
+		frame_cvt4=np.copy(frame_cvt3)
+		frame_cvt=cv2.GaussianBlur(frame_cvt3,(3,3),0)
 		edges = cv2.Canny(frame_cvt,86,270) #found out using hit and try using the sliders , need some fine tuning
 		# edges = cv2.Canny(frame_cvt,90,180)
 		#these parameters need tu be fine tuned
 		lines = cv2.HoughLinesP(edges,10,np.pi/180,100,np.array([]),170,5)
 
 		draw_lines(frame_cvt,lines,frame_cvt2)
-		# for a in coordinates[0]:
-		# 	image=cv2.circle(frame_cvt,(a[0],a[1]),25,(255,255,255),5)
+		for a in coordinates[0]:
+			image=cv2.circle(frame_cvt4,(a[0],a[1]),25,(255,255,255),5)
 
 
 		temp=roi(edges,coordinates)
 		
 		contours,hierarchy = cv2.findContours(temp,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
-
-
-
-		# for a in contours:
-		# 	x,y,w,h = cv2.boundingRect(a)
-		# 	cv2.rectangle(frame_cvt,(x,y),(x+w,y+h),(255,255,255),2)
-
-		# areas = [cv2.contourArea(c) for c in contours]
-		# try:
-		# 	max_index = np.argmax(areas)
-		# except ValueError:
-		# 	continue
-
-		# cnt=contours[max_index] #Biggest contour
-		# x,y,w,h = cv2.boundingRect(cnt)
-		# cv2.rectangle(frame_cvt,(x,y),(x+w,y+h),(255,255,0),2)
 
 		contour_list = []
 		for contour in contours:
@@ -140,19 +133,22 @@ if __name__=='__main__':
 
 		for a in contour_list:
 			x,y,w,h = cv2.boundingRect(a)
-			cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),2)
-			# cv2.drawContours(frame_cvt2, contour_list,  -1, (255,0,0), 2)
-			
-		# cv2.imshow("frame_cvt",frame_cvt)
-		# cv2.imshow("frame_cvt2",frame_cvt2)
-		# cv2.imshow("temp",temp)
-		cv2.imshow("temp",frame)
+			cv2.rectangle(frame_final_output,(x,y),(x+w,y+h),(255,255,255),2)
+
+		cv2.drawContours(frame_cvt2, contour_list,  -1, (255,0,0), 2)
+
+		cv2.imshow("frame_final_output",frame_final_output)
 		fps=1.0/(time.time()-last_time)
 		print("FPS =",fps)
 		last_time=time.time()
-		if cv2.waitKey(25) & 0xFF == ord('q'):
+		frame_list=[frame,frame_cvt3,edges,frame_cvt4,temp,frame_cvt2,frame_final_output]
+		key=cv2.waitKey(25)
+		if key == ord('q'):
 			cv2.destroyAllWindows()
 			break
+		elif key==ord("j"):
+			capture(frame_list)
+			i+=1
 
 	done.set()
 
